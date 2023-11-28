b3b3eda73606d5167f3ec28dc724d0e9e49df32f
Christopher Ball <chris.m.ball@gmail.com>
2023-11-27 17:53:04 -0800

    Fixed formatting and silencing a couple ts errors.

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,7 @@
-// This file will add both p5 instanced and global intellisence 
-import * as p5Global from 'p5/global' 
-import module = require('p5');
+// This file adds p5 instanced intellisence.
+// I've commented out the global flavor of
+// intellisense to reduce confusion.
+// import * as p5Global from "p5/global";
+import module = require("p5");
 export = module;
 export as namespace p5;
-
