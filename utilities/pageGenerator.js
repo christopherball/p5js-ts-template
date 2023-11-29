10ae2c4e28f0270d0a4631a13c68b29e48f1ddb2
Christopher Ball <chris.m.ball@gmail.com>
2023-11-29 11:07:57 -0800

    Wrote custom html page generator using handlebars.

---
+++ utilities/pageGenerator.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+var Handlebars = require("handlebars");
+
+function render(filename, data) {
+  var source = fs.readFileSync(filename, "utf8").toString();
+  var template = Handlebars.compile(source);
+  var output = template(data);
+  return output;
+}
+
+var data = JSON.parse(fs.readFileSync("./src/page.json", "utf8"));
+
+for (var i = 0; i < data.page.length; i++) {
+  var result = render("./src/page.handlebars", data.page[i]);
+  // console.log(result);
+  // console.log();
+  fs.writeFileSync(
+    "./dist/" + data.page[i].scriptFilenamePrefix + ".html",
+    result
+  );
+}
